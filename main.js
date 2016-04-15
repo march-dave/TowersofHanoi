bb8291c3ecd012824c55ace4bd4999594726e3e4
march-dave <ysangy@gmail.com>
2016-04-15 02:35:29 -0700

    MVP done

--- main.js
+++ main.js
@@ -7,12 +7,22 @@ function init() {
 }
 
 var $selectedBlock = "";
+var selectedBlockNum = 0;
 function clickShell(event) {
 
 	if ($selectedBlock.length) { 
 		console.log("selected > 0");
 
 		var $selectedShell = $(this).find('.towerBase');
+
+		var numInTower = parseInt($selectedShell.first().text());
+		selectedBlockNum = $selectedBlock[0].textContent;
+
+		if (numInTower < parseInt(selectedBlockNum) ) {
+			console.log("Do not move block!");
+			return false;
+		}
+
 		$($selectedShell).prepend($selectedBlock);
 
 		$selectedBlock = "";
